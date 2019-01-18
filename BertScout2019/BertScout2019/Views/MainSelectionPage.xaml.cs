f492af559dded4a794e7e685a9f762229d78c196
Scott Bakker <scott@bakkergames.com>
2019-01-17 19:55:08 -0500

    hello on button click

--- BertScout2019/BertScout2019/Views/MainSelectionPage.xaml.cs
+++ BertScout2019/BertScout2019/Views/MainSelectionPage.xaml.cs
@@ -9,12 +9,26 @@ using Xamarin.Forms.Xaml;
 
 namespace BertScout2019.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MainSelectionPage : ContentPage
-	{
-		public MainSelectionPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainSelectionPage : ContentPage
+    {
+        public MainSelectionPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Select_FRC_Event_Clicked(object sender, EventArgs e)
+        {
+            if (!((Button)sender).Text.EndsWith("Hello!") 
+                && 
+                !((Button)sender).Text.StartsWith("Please"))
+            {
+                ((Button)sender).Text += " - Hello!";
+            }
+            else
+            {
+                ((Button)sender).Text = "Please don't click me again!";
+            }
+        }
+    }
 }

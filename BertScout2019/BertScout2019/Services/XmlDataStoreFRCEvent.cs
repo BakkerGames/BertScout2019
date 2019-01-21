b6c0f85a92e64aef87606b46e1e4cfe27537d83d
Scott Bakker <scott@bakkergames.com>
2019-01-21 11:18:57 -0500

    working xml event load!

---
+++ BertScout2019/BertScout2019/Services/XmlDataStoreFRCEvent.cs
@@ -0,0 +1,58 @@
+﻿using BertScout2019.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BertScout2019.Services
+{
+    public class XmlDataStoreFRCEvent : IDataStore<FRCEvent>
+    {
+        List<FRCEvent> items;
+
+        public XmlDataStoreFRCEvent()
+        {
+            items = new List<FRCEvent>();
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("BertScout2019.EmbeddedResources.FRCEvents.xml");
+            using (var reader = new StreamReader(stream))
+            {
+                var serializer = new XmlSerializer(typeof(List<FRCEvent>));
+                items = (List<FRCEvent>)serializer.Deserialize(reader);
+            }
+        }
+
+        public async Task<bool> AddItemAsync(FRCEvent item)
+        {
+            items.Add(item);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteItemAsync(string id)
+        {
+            var oldItem = items.Where((FRCEvent arg) => arg.Id == id).FirstOrDefault();
+            items.Remove(oldItem);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<FRCEvent> GetItemAsync(string id)
+        {
+            return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
+        }
+
+        public async Task<IEnumerable<FRCEvent>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(items);
+        }
+
+        public async Task<bool> UpdateItemAsync(FRCEvent item)
+        {
+            var oldItem = items.Where((FRCEvent arg) => arg.Id == item.Id).FirstOrDefault();
+            items.Remove(oldItem);
+            items.Add(item);
+            return await Task.FromResult(true);
+        }
+    }
+}
--- BertScout2019/BertScout2019/ViewModels/SelectEventsViewModel.cs
+++ BertScout2019/BertScout2019/ViewModels/SelectEventsViewModel.cs
@@ -1,17 +1,21 @@
 ﻿using BertScout2019.Models;
 using BertScout2019.Services;
-using BertScout2019.Views;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using Xamarin.Forms;
 
 namespace BertScout2019.ViewModels
 {
     public class SelectEventsViewModel : BaseViewModel
     {
-        public IDataStore<FRCEvent> DataStoreFRCEvent => DependencyService.Get<IDataStore<FRCEvent>>() ?? new MockDataStoreFRCEvent();
+        //public IDataStore<FRCEvent> DataStoreFRCEvent => DependencyService.Get<IDataStore<FRCEvent>>() ?? new DataStoreFRCEvent();
+        public IDataStore<FRCEvent> DataStoreFRCEvent => new XmlDataStoreFRCEvent();
 
         public ObservableCollection<FRCEvent> FRCEvents { get; set; }
         public Command LoadFRCEventsCommand { get; set; }
@@ -40,11 +44,42 @@ namespace BertScout2019.ViewModels
             try
             {
                 FRCEvents.Clear();
+
+
+                //var assembly = typeof(App).GetTypeInfo().Assembly;
+                //Stream stream = assembly.GetManifestResourceStream("BertScout2019.EmbeddedResources.FRCEvents.xml");
+                //using (var reader = new StreamReader(stream))
+                //{
+                //    var serializer = new XmlSerializer(typeof(ObservableCollection<FRCEvent>));
+                //    FRCEvents = (ObservableCollection<FRCEvent>)serializer.Deserialize(reader);
+                //}
+                //foreach (FRCEvent obj in FRCEvents)
+                //{
+                //    Console.WriteLine(obj.Name);
+                //}
+                //Console.ReadLine();
+                //var assembly = IntrospectionExtensions.GetTypeInfo(typeof(App)).Assembly;
+                //Stream stream = assembly.GetManifestResourceStream("WorkingWithFiles.PCLXmlResource.xml");
+                //List<FRCEvent> FRCEvents;
+                //using (var reader = new System.IO.StreamReader(stream))
+                //{
+                //    var serializer = new XmlSerializer(typeof(List<FRCEvent>));
+                //    FRCEvents = (List<FRCEvent>)serializer.Deserialize(reader);
+                //}
+                //var listView = new ListView();
+                //listView.ItemsSource = FRCEvents;
+
+
+
                 var items = await DataStoreFRCEvent.GetItemsAsync(true);
                 foreach (var item in items)
                 {
                     FRCEvents.Add(item);
                 }
+                //XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<FRCEvent>));
+                //StringWriter sw = new StringWriter();
+                //serializer.Serialize(sw, FRCEvents);
+                //Console.WriteLine(sw.ToString());
             }
             catch (Exception ex)
             {

708cf2aa9f14bf7fd839e3fa853869a08603dc71
Scott Bakker <scott@bakkergames.com>
2019-01-22 12:31:10 -0500

    eventteammatch model

---
+++ BertScout2019/BertScout2019/Models/EventTeamMatch.cs
@@ -0,0 +1,37 @@
+﻿namespace BertScout2019.Models
+{
+    public class EventTeamMatch
+    {
+        public string Id { get; set; }
+        public string EventID { get; set; }
+        public string TeamID { get; set; }
+        public int Match { get; set; }
+
+        public int SandstormMoveType { get; set; }
+        public int SandstormOffPlatform { get; set; }
+        public int SandstormHatches { get; set; }
+        public int SandstormCargo { get; set; }
+
+        public int CargoShipHatches { get; set; }
+        public int CargoShipCargo { get; set; }
+        public int RocketHatches { get; set; }
+        public int RocketCargo { get; set; }
+        public int RocketHighestHatch { get; set; }
+        public int RocketHighestCargo { get; set; }
+
+        public int EndgamePlatform { get; set; }
+        public int EndgameBuddyClimb { get; set; }
+
+        public int Defense { get; set; }
+        public int Cooperation { get; set; }
+        public int Fouls { get; set; }
+        public int Broken { get; set; }
+
+        public int AllianceScore { get; set; }
+        public int LostTiedWon { get; set; }
+        public int RocketRankingPoint { get; set; }
+        public int HabRankingPoint { get; set; }
+
+        public string Comments { get; set; }
+    }
+}

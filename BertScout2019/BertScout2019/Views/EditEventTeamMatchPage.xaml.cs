5c0acbbb01de69c224a1cf2b21047f0d461f97db
Scott Bakker <scott@bakkergames.com>
2019-01-28 16:00:12 -0500

    all sandstorm fields working!

--- BertScout2019/BertScout2019/Views/EditEventTeamMatchPage.xaml.cs
+++ BertScout2019/BertScout2019/Views/EditEventTeamMatchPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using BertScout2019.Models;
+﻿using BertScout2019.Models;
 using BertScout2019.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -20,16 +19,24 @@ namespace BertScout2019.Views
             SetButtons();
         }
 
+        //protected override void OnAppearing()
+        //{
+        //    base.OnAppearing();
+        //}
+
         private void SetButtons()
         {
-            SandstormMovementType = viewModel.item.SandstormMoveType;
+            SandstormMoveType = viewModel.item.SandstormMoveType;
+            SandstormOffPlatform = viewModel.item.SandstormOffPlatform;
+            SandstormHatches = viewModel.item.SandstormHatches;
+            SandstormCargo = viewModel.item.SandstormCargo;
 
             //todo add more here
         }
 
-        #region SandstormMovementType
+        #region SandstormMoveType
 
-        public int SandstormMovementType
+        public int SandstormMoveType
         {
             get
             {
@@ -42,21 +49,21 @@ namespace BertScout2019.Views
                 {
                     case 1:
                         viewModel.item.SandstormMoveType = 1;
-                        Button_MovementType_None.BackgroundColor = App.UnselectedButtonColor;
-                        Button_MovementType_Tele.BackgroundColor = App.SelectedButtonColor;
-                        Button_MovementType_Auto.BackgroundColor = App.UnselectedButtonColor;
+                        Button_SandstormMoveType_None.BackgroundColor = App.UnselectedButtonColor;
+                        Button_SandstormMoveType_Tele.BackgroundColor = App.SelectedButtonColor;
+                        Button_SandstormMoveType_Auto.BackgroundColor = App.UnselectedButtonColor;
                         break;
                     case 2:
                         viewModel.item.SandstormMoveType = 2;
-                        Button_MovementType_None.BackgroundColor = App.UnselectedButtonColor;
-                        Button_MovementType_Tele.BackgroundColor = App.UnselectedButtonColor;
-                        Button_MovementType_Auto.BackgroundColor = App.SelectedButtonColor;
+                        Button_SandstormMoveType_None.BackgroundColor = App.UnselectedButtonColor;
+                        Button_SandstormMoveType_Tele.BackgroundColor = App.UnselectedButtonColor;
+                        Button_SandstormMoveType_Auto.BackgroundColor = App.SelectedButtonColor;
                         break;
                     default:
                         viewModel.item.SandstormMoveType = 0;
-                        Button_MovementType_None.BackgroundColor = App.SelectedButtonColor;
-                        Button_MovementType_Tele.BackgroundColor = App.UnselectedButtonColor;
-                        Button_MovementType_Auto.BackgroundColor = App.UnselectedButtonColor;
+                        Button_SandstormMoveType_None.BackgroundColor = App.SelectedButtonColor;
+                        Button_SandstormMoveType_Tele.BackgroundColor = App.UnselectedButtonColor;
+                        Button_SandstormMoveType_Auto.BackgroundColor = App.UnselectedButtonColor;
                         break;
                 }
                 if (saveValue != viewModel.item.SandstormMoveType)
@@ -66,19 +73,149 @@ namespace BertScout2019.Views
             }
         }
 
-        private void Button_MovementType_None_Clicked(object sender, System.EventArgs e)
+        private void Button_SandstormMoveType_None_Clicked(object sender, System.EventArgs e)
         {
-            SandstormMovementType = 0;
+            SandstormMoveType = 0;
         }
 
-        private void Button_MovementType_Tele_Clicked(object sender, System.EventArgs e)
+        private void Button_SandstormMoveType_Tele_Clicked(object sender, System.EventArgs e)
         {
-            SandstormMovementType = 1;
+            SandstormMoveType = 1;
         }
 
-        private void Button_MovementType_Auto_Clicked(object sender, System.EventArgs e)
+        private void Button_SandstormMoveType_Auto_Clicked(object sender, System.EventArgs e)
         {
-            SandstormMovementType = 2;
+            SandstormMoveType = 2;
+        }
+
+        #endregion
+
+        #region SandstormOffPlatform
+
+        public int SandstormOffPlatform
+        {
+            get
+            {
+                return viewModel.item.SandstormOffPlatform;
+            }
+            set
+            {
+                int saveValue = viewModel.item.SandstormOffPlatform;
+                switch (value)
+                {
+                    case 1:
+                        viewModel.item.SandstormOffPlatform = 1;
+                        Button_SandstormOffPlatform_None.BackgroundColor = App.UnselectedButtonColor;
+                        Button_SandstormOffPlatform_1.BackgroundColor = App.SelectedButtonColor;
+                        Button_SandstormOffPlatform_2.BackgroundColor = App.UnselectedButtonColor;
+                        break;
+                    case 2:
+                        viewModel.item.SandstormOffPlatform = 2;
+                        Button_SandstormOffPlatform_None.BackgroundColor = App.UnselectedButtonColor;
+                        Button_SandstormOffPlatform_1.BackgroundColor = App.UnselectedButtonColor;
+                        Button_SandstormOffPlatform_2.BackgroundColor = App.SelectedButtonColor;
+                        break;
+                    default:
+                        viewModel.item.SandstormOffPlatform = 0;
+                        Button_SandstormOffPlatform_None.BackgroundColor = App.SelectedButtonColor;
+                        Button_SandstormOffPlatform_1.BackgroundColor = App.UnselectedButtonColor;
+                        Button_SandstormOffPlatform_2.BackgroundColor = App.UnselectedButtonColor;
+                        break;
+                }
+                if (saveValue != viewModel.item.SandstormOffPlatform)
+                {
+                    App.database.SaveEventTeamMatchAsync(viewModel.item);
+                }
+            }
+        }
+
+        private void Button_SandstormOffPlatform_None_Clicked(object sender, System.EventArgs e)
+        {
+            SandstormOffPlatform = 0;
+        }
+
+        private void Button_SandstormOffPlatform_1_Clicked(object sender, System.EventArgs e)
+        {
+            SandstormOffPlatform = 1;
+        }
+
+        private void Button_SandstormOffPlatform_2_Clicked(object sender, System.EventArgs e)
+        {
+            SandstormOffPlatform = 2;
+        }
+
+        #endregion
+
+        #region SandstormHatches
+
+        public int SandstormHatches
+        {
+            get
+            {
+                return viewModel.item.SandstormHatches;
+            }
+            set
+            {
+                Label_SandstormHatches_Value.Text = value.ToString();
+                if (viewModel.item.SandstormHatches != value)
+                {
+                    viewModel.item.SandstormHatches = value;
+                    App.database.SaveEventTeamMatchAsync(viewModel.item);
+                }
+            }
+        }
+
+        private void Button_SandstormHatches_Minus_Clicked(object sender, System.EventArgs e)
+        {
+            if (SandstormHatches > 0)
+            {
+                SandstormHatches--;
+            }
+        }
+
+        private void Button_SandstormHatches_Plus_Clicked(object sender, System.EventArgs e)
+        {
+            if (SandstormHatches < 99)
+            {
+                SandstormHatches++;
+            }
+        }
+
+        #endregion
+
+        #region SandstormCargo
+
+        public int SandstormCargo
+        {
+            get
+            {
+                return viewModel.item.SandstormCargo;
+            }
+            set
+            {
+                Label_SandstormCargo_Value.Text = value.ToString();
+                if (viewModel.item.SandstormCargo != value)
+                {
+                    viewModel.item.SandstormCargo = value;
+                    App.database.SaveEventTeamMatchAsync(viewModel.item);
+                }
+            }
+        }
+
+        private void Button_SandstormCargo_Minus_Clicked(object sender, System.EventArgs e)
+        {
+            if (SandstormCargo > 0)
+            {
+                SandstormCargo--;
+            }
+        }
+
+        private void Button_SandstormCargo_Plus_Clicked(object sender, System.EventArgs e)
+        {
+            if (SandstormCargo < 99)
+            {
+                SandstormCargo++;
+            }
         }
 
         #endregion

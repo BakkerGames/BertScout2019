5458a897dcb045b0159b75b1d37e4b671b8c137b
Chloe Falkner <chloefalkner@gmail.com>
2019-02-02 17:51:28 -0500

    add new team page now has stuff

--- BertScout2019/BertScout2019/Views/AddNewTeamPage.xaml.cs
+++ BertScout2019/BertScout2019/Views/AddNewTeamPage.xaml.cs
@@ -16,5 +16,45 @@ namespace BertScout2019.Views
 		{
 			InitializeComponent ();
 		}
+
+        private void Add_New_Team_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Add_NewTeam_Clicked(object sender, EventArgs e)
+        {
+            /*int value = int.Parse(TeamNumberLabelValue.Text);
+            foreach (TeamNumber oldTeam in viewModel.Teams)
+            {
+                if (oldTeam.TeamNumber == value)
+                {
+                    return;
+                }
+            }
+            TeamNumber newTeam = new TeamNumber();
+            newTeam.EventKey = App.currFRCEventKey;
+            newTeam.TeamNumber = App.currTeamNumber;
+            App.database.SaveTeamNumberAsync(newTeam);
+            if (App.highestTeamNumber < value)
+            {
+                App.highestTeamNumber = value;
+            }
+            // add new team into list in proper order
+            bool found = false;
+            for (int i = 0; i < viewModel.Teams.Count; i++)
+            {
+                if (viewModel.Teams[i].TeamNumber > value)
+                {
+                    found = true;
+                    viewModel.Teams.Insert(i, newTeam);
+                    break;
+                }
+            }
+            if (!found)
+            {
+                viewModel.TeamNumber.Add(newTeam);
+            }*/
+        }
     }
 }

909fe4f1d0e834ad0a63645ff5e1d7d563637bf0
Scott Bakker <scott@bakkergames.com>
2019-02-02 14:51:28 -0500

    TEAM DETAILS Are somewhat there, appearantly we are in anytownusa

---
+++ BertScout2019/BertScout2019/ViewModels/TeamDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BertScout2019.ViewModels
+{
+    public class TeamDetailViewModel : BaseViewModel
+    {
+        public TeamDetailViewModel()
+        {
+            Title = "Team Details";
+        }
+    }
+}
--- BertScout2019/BertScout2019/Views/TeamDetailsPage.xaml.cs
+++ BertScout2019/BertScout2019/Views/TeamDetailsPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BertScout2019.Models;
+using BertScout2019.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,24 @@ namespace BertScout2019.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TeamDetailsPage : ContentPage
     {
+        Team team;
+
+        TeamDetailViewModel viewModel;
+
         public TeamDetailsPage()
         {
             InitializeComponent();
+
+            BindingContext = viewModel = new TeamDetailViewModel();
+        }
+
+
+
+        protected override void OnAppearing()
+        {
+            TeamDetails_Number.Text = App.currTeamNumber.ToString();
+            TeamDetails_Name.Text = App.currTeamName;
+            TeamDetails_Location.Text = "anytownusa";
         }
     }
 }

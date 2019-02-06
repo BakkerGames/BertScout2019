54bc65c8a7dd5d5d97331189a282aa94898af2ab
Scott Bakker <sbakker343@gmail.com>
2019-02-05 19:53:13 -0500

    added match results Hello Nate!

---
+++ BertScout2019/BertScout2019/ViewModels/MatchResultsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BertScout2019.Models;
+using System.Collections.ObjectModel;
+
+namespace BertScout2019.ViewModels
+{
+    public class MatchResultsViewModel : BaseViewModel
+    {
+        public ObservableCollection<MatchResult> MatchResults { get; set; }
+    }
+}
--- BertScout2019/BertScout2019/Views/TeamDetailsPage.xaml.cs
+++ BertScout2019/BertScout2019/Views/TeamDetailsPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using BertScout2019.Models;
 using BertScout2019.ViewModels;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -22,6 +24,13 @@ namespace BertScout2019.Views
             TeamDetails_Number.Text = viewModel.item.TeamNumber.ToString(); //App.currTeamNumber.ToString();
             TeamDetails_Name.Text = viewModel.item.Name; // App.currTeamName;
             TeamDetails_Location.Text = viewModel.item.Location; //"anytownusa";
+            viewModel.MatchResults.Add(new MatchResult() { Text = "Hello" });
+            viewModel.MatchResults.Add(new MatchResult() { Text = "Nate!" });
+        }
+
+        private void TeamMatchesListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
         }
     }
 }

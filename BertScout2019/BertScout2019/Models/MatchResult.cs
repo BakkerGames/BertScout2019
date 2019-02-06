4935d13e92a1d06ca9140e011215e29cb83a7193
Scott Bakker <sbakker343@gmail.com>
2019-02-05 20:23:28 -0500

    second line of match results

--- BertScout2019/BertScout2019/Models/MatchResult.cs
+++ BertScout2019/BertScout2019/Models/MatchResult.cs
@@ -6,6 +6,7 @@ namespace BertScout2019.Models
 {
     public class MatchResult
     {
-        public string Text { get; set; }
+        public string Text1 { get; set; }
+        public string Text2 { get; set; }
     }
 }
--- BertScout2019/BertScout2019/Views/TeamDetailsPage.xaml.cs
+++ BertScout2019/BertScout2019/Views/TeamDetailsPage.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using BertScout2019.Models;
 using BertScout2019.ViewModels;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -24,15 +22,11 @@ namespace BertScout2019.Views
             TeamDetails_Number.Text = viewModel.item.TeamNumber.ToString(); //App.currTeamNumber.ToString();
             TeamDetails_Name.Text = viewModel.item.Name; // App.currTeamName;
             TeamDetails_Location.Text = viewModel.item.Location; //"anytownusa";
-
-
-            //viewModel.MatchResults.Add(new MatchResult() { Text = "Hello" });
-            //viewModel.MatchResults.Add(new MatchResult() { Text = "Nate!" });
         }
 
         private void TeamMatchesListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-
+            // todo make something happen when select result
         }
     }
 }

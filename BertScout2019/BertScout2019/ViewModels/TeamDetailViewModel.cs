8b4d1b7766378d24ef9899febb08f8ca1dd7ddce
Scott Bakker <sbakker343@gmail.com>
2019-02-05 20:15:34 -0500

    working match results (dummy data)

--- BertScout2019/BertScout2019/ViewModels/TeamDetailViewModel.cs
+++ BertScout2019/BertScout2019/ViewModels/TeamDetailViewModel.cs
@@ -1,11 +1,16 @@
 ﻿using BertScout2019.Models;
+using BertScout2019.Services;
+using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace BertScout2019.ViewModels
 {
     public class TeamDetailViewModel : BaseViewModel
     {
         public Team item;
+
+        public IDataStore<EventTeamMatch> DataStoreMatch;
 
         public ObservableCollection<MatchResult> MatchResults { get; set; }
 
@@ -14,6 +19,37 @@ namespace BertScout2019.ViewModels
             this.item = item;
             Title = $"Team {App.currTeamNumber} Details";
             MatchResults = new ObservableCollection<MatchResult>();
+            DataStoreMatch = new SqlDataStoreMatchesByEventTeam(App.currFRCEventKey, item.TeamNumber);
+            ExecuteLoadEventTeamMatchesCommand();
         }
+
+        public void ExecuteLoadEventTeamMatchesCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                MatchResults.Clear();
+                var matches = DataStoreMatch.GetItemsAsync(true).Result;
+                foreach (var match in matches)
+                {
+                    MatchResult obj = new MatchResult();
+                    obj.Text = $"Match {match.MatchNumber}";
+                    MatchResults.Add(obj);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
     }
 }

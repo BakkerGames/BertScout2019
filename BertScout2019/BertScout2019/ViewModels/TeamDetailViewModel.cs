192ba87f0d78d7257bdd480d7d65ee725e79c960
Nate <nategagnon101@gmail.com>
2019-02-05 19:15:43 -0500

    finished fixing merge, ahhhhhhhhh

--- BertScout2019/BertScout2019/ViewModels/TeamDetailViewModel.cs
+++ BertScout2019/BertScout2019/ViewModels/TeamDetailViewModel.cs
@@ -4,27 +4,13 @@ namespace BertScout2019.ViewModels
 {
     public class TeamDetailViewModel : BaseViewModel
     {
-<<<<<<< HEAD
-        //do not edit 
-        //public IDataStore<Team> DataStoreTeam => new SqlDataStoreEventTeams(App.currFRCEventKey);
 
-        //try to edit 
-        //public <Location> Location => new SqlDataStoreTeamLoc(App.currFRCTeamLoc);
-        //public IDataStore<Team> DataStoreTeams => new SqlDataStoreTeams();
-
-        public TeamDetailViewModel()
-        {
-            Title = "Team Details";
-
-            
-=======
         public Team item;
 
         public TeamDetailViewModel(Team item)
         {
             this.item = item;
             Title = $"Team {App.currTeamNumber} Details";
->>>>>>> master
         }
     }
 }

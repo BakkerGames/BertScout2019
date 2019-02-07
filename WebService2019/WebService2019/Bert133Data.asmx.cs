7d29a09243a52f0e148710568679e93467bd7367
Scott Bakker <sbakker343@gmail.com>
2019-02-07 11:40:52 -0500

    use BertScout2019Data sqlite

--- WebService2019/WebService2019/Bert133Data.asmx.cs
+++ WebService2019/WebService2019/Bert133Data.asmx.cs
@@ -1,12 +1,6 @@
-﻿using BertScout2019.Data;
-using BertScout2019.Models;
-using Common.JSON;
+﻿using BertScout2019Data.Data;
 using System;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
 using System.IO;
-using System.Text;
 using System.Web.Services;
 
 namespace WebService2019
@@ -22,8 +16,6 @@ namespace WebService2019
     public class Bert133Data : WebService
     {
 
-        //private int _timeout = 30;
-
         // app database
         private const string dbFilename = "bertscout2019.db3";
         public static BertScout2019Database _database;
@@ -34,7 +26,8 @@ namespace WebService2019
             {
                 if (_database == null)
                 {
-                    _database = new BertScout2019Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), dbFilename));
+                    string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), dbFilename);
+                    _database = new BertScout2019Database(dbPath);
                 }
                 return _database;
             }
@@ -53,25 +46,6 @@ namespace WebService2019
             {
                 return ex.Message;
             }
-            //string connectionString = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
-            //using (SqlConnection dc = new SqlConnection(connectionString))
-            //{
-            //    dc.Open();
-            //    StringBuilder query = new StringBuilder();
-            //    // todo drop, create, and fill all tables
-            //    //query.AppendLine("TRUNCATE TABLE [dbo].[FRCEvent];");
-            //    //query.AppendLine("TRUNCATE TABLE [dbo].[Team];");
-            //    //query.AppendLine("TRUNCATE TABLE [dbo].[EventTeam];");
-            //    query.AppendLine("TRUNCATE TABLE [dbo].[EventTeamMatch];");
-            //    SqlCommand cmd = new SqlCommand(query.ToString(), dc)
-            //    {
-            //        CommandType = CommandType.Text,
-            //        CommandTimeout = _timeout,
-            //    };
-            //    int result = cmd.ExecuteNonQuery();
-            //    return $"Database reset done, result = {result}";
-            //}
-            //return "Not implemented";
         }
 
         [WebMethod]

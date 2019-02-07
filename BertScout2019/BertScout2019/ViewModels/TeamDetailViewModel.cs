6d5ff3bbbd5020f696a186496347dc66493e708b
Nate <nategagnon101@gmail.com>
2019-02-06 20:25:46 -0500

    Working Score Pagegit statusgit statusgit statusgit statusgit statusgit statusgit statusgit statusgit statusgit statusgit statusgit status

--- BertScout2019/BertScout2019/ViewModels/TeamDetailViewModel.cs
+++ BertScout2019/BertScout2019/ViewModels/TeamDetailViewModel.cs
@@ -40,7 +40,7 @@ namespace BertScout2019.ViewModels
                     MatchResult obj = new MatchResult();
                     // todo fill in text with useful match results
                     obj.Text1 = $"Match {match.MatchNumber}";
-                    obj.Text2 = "Hello Nate";
+                    obj.Text2 = CalculateMatchResult(match);
                     MatchResults.Add(obj);
                 }
             }
@@ -54,5 +54,39 @@ namespace BertScout2019.ViewModels
             }
         }
 
+        private string CalculateMatchResult(EventTeamMatch match)
+        {
+            string result;
+            int score = 0;
+            score += match.SandstormMoveType;
+            score += match.SandstormOffPlatform;
+            score += match.SandstormHatches;
+            score += match.SandstormCargo;
+
+            score += match.CargoShipHatches;
+            score += match.CargoShipCargo;
+            score += match.RocketHatches;
+            score += match.RocketCargo;
+            score += match.RocketHighestHatch;
+            score += match.RocketHighestCargo;
+
+            score += match.EndgamePlatform;
+            score += match.EndgameBuddyClimb;
+
+            score += match.Defense;
+            score += match.Cooperation;
+            score -= match.Fouls*10;
+            score -= match.Broken*20;
+
+            int rp = 0;
+
+            rp += match.AllianceResult;
+            rp += match.RocketRankingPoint;
+            rp += match.HabRankingPoint;
+            result = $"Score: {score} RP: {rp}";
+            // todo add score and ranking points
+
+            return result;
+        }
     }
 }

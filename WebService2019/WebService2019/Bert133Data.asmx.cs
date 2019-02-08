5b3aa016a6e2812a9d8a3ba36a24e1efa2312ba2
Scott Bakker <sbakker343@gmail.com>
2019-02-07 19:58:52 -0500

    fixing #42...

--- WebService2019/WebService2019/Bert133Data.asmx.cs
+++ WebService2019/WebService2019/Bert133Data.asmx.cs
@@ -17,7 +17,7 @@ namespace WebService2019
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    //[System.Web.Script.Services.ScriptService]
+    [System.Web.Script.Services.ScriptService]
     public class Bert133Data : WebService
     {
 

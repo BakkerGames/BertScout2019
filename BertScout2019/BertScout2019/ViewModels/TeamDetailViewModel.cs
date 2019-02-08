5c6a4b8c67840f4679660317bf54d60c8a43e384
Scott Bakker <sbakker343@gmail.com>
2019-02-07 19:47:51 -0500

    merging nate's changes

--- BertScout2019/BertScout2019/ViewModels/TeamDetailViewModel.cs
+++ BertScout2019/BertScout2019/ViewModels/TeamDetailViewModel.cs
@@ -12,6 +12,8 @@ namespace BertScout2019.ViewModels
 
         public Team item;
 
+        public int TotalRP = 0;
+
         public IDataStore<EventTeamMatch> DataStoreMatch;
 
         public ObservableCollection<MatchResult> MatchResults { get; set; }
@@ -42,6 +44,7 @@ namespace BertScout2019.ViewModels
                     // todo fill in text with useful match results
                     obj.Text1 = $"Match {match.MatchNumber}";
                     obj.Text2 = CalculateMatchResult(match);
+                    TotalRP +=CalculateMatchRP(match);
                     MatchResults.Add(obj);
                 }
             }
@@ -54,6 +57,61 @@ namespace BertScout2019.ViewModels
                 IsBusy = false;
             }
         }
+        private int CalculateMatchRP(EventTeamMatch match)
+        {
+            int rp = 0;
+
+            rp += match.AllianceResult;
+            rp += match.RocketRankingPoint;
+            rp += match.HabRankingPoint;
+
+            return rp;
+
+
+
+
+        }
+
+            private string CalculateMatchResult(EventTeamMatch match)
+        {
+            string result;
+            int score = 0;
+            //not scoring movement type
+            //score += match.SandstormMoveType;
+            score += match.SandstormOffPlatform*3;
+            score += match.SandstormHatches*2;
+            score += match.SandstormCargo*3;
+
+            score += match.CargoShipHatches*2;
+            score += match.CargoShipCargo*3;
+            score += match.RocketHatches*2;
+            score += match.RocketCargo*3;
+            //not scoring highest platform
+            //score += match.RocketHighestHatch;
+            //score += match.RocketHighestCargo;
+
+            //score += match.EndgamePlatform;
+            switch (match.EndgamePlatform) {
+                case 1:
+                    score += 3;
+                    break;
+                case 2:
+                    score += 6;
+                    break;
+                case 3:
+                    score += 12;
+                    break;
+            }
+            //not scoring buddy climb
+            //score += match.EndgameBuddyClimb;
+
+            //score += match.Defense;
+            //score += match.Cooperation;
+            score -= match.Fouls*3;
+            score -= match.TechFouls*10;
+            //score -= match.Broken*20;
+
+            int rp = 0;
 
         private string CalculateMatchResult(EventTeamMatch match)
         {

a5fbb8de4146a5d3947c4814768cf42eac91f617
Scott Bakker <sbakker343@gmail.com>
2019-02-07 20:18:47 -0500

    add chloe's changes

---
+++ BertScout2019/BertScout2019/Views/AddNewTeamPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BertScout2019.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AddNewTeamPage : ContentPage
+	{
+		public AddNewTeamPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Add_New_Team_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Add_NewTeam_Clicked(object sender, EventArgs e)
+        {
+            int newTeamNumber = 0;
+            if (Add_New_Team.Text == "")
+            {
+                this.Title = "Must Specify Team Number";
+                return;
+            }
+            if (!int.TryParse(Add_New_Team.Text, out newTeamNumber))
+            {
+                this.Title = "Numbers Only Please!";
+                return;
+            }
+            if (newTeamNumber > 9999 || newTeamNumber < 1)
+            {
+                this.Title = "Number out of range";
+                return;
+            }
+            //if (App.database.GetTeamsAsync(newTeamNumber))
+            //{
+            //    this.Title = "Team Already Exists";
+            //    return;
+            //}
+                this.Title = $"Added new team {newTeamNumber}";
+        }
+    }
+}

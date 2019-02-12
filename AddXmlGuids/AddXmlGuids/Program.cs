05c329ed086281469e375486ff81f28516057a7a
Scott Bakker <sbakker343@gmail.com>
2019-02-11 20:22:23 -0500

    giving all xml items a guid

---
+++ AddXmlGuids/AddXmlGuids/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AddXmlGuids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool fixFlag = false;
+            string basePath = "D:\\Projects\\BertScout2019\\BertScout2019XmlData\\EmbeddedResources";
+            foreach (string filename in Directory.GetFiles(basePath))
+            {
+                fixFlag = false;
+                if (filename.EndsWith(".xml"))
+                {
+                    string[] lines = File.ReadAllLines(filename);
+                    for (int i = 0; i < lines.GetLength(0); i++)
+                    {
+                        if (lines[i] == "    <Id>0</Id>")
+                        {
+                            lines[i] = $"    <Uuid>{Guid.NewGuid()}</Uuid>";
+                            Console.WriteLine(lines[i]);
+                            fixFlag = true;
+                        }
+                    }
+                    if (fixFlag)
+                    {
+                        File.Move(filename, filename + ".old");
+                        File.WriteAllLines(filename, lines);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}

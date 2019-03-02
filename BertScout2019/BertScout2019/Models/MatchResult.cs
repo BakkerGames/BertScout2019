87bb4791b79ea67cae20f4efe2ac14bf6344f4f2
Scott Bakker <scott@bakkergames.com>
2019-03-02 10:42:49 -0500

    select team from results page

--- BertScout2019/BertScout2019/Models/MatchResult.cs
+++ BertScout2019/BertScout2019/Models/MatchResult.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BertScout2019.Models
+﻿namespace BertScout2019.Models
 {
     public class MatchResult
     {
--- BertScout2019/BertScout2019/Views/ResultPage.xaml.cs
+++ BertScout2019/BertScout2019/Views/ResultPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿// For dropdown menu-use picker, info at https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/picker/
 
 using System;
+using BertScout2019.Models;
+using BertScout2019.Services;
 using BertScout2019.ViewModels;
 using BertScout2019Data.Models;
 using Xamarin.Forms;
@@ -30,14 +32,15 @@ namespace BertScout2019.Views
 
         private async void EventTeamsListView_ItemSelected(object sender, SelectedItemChangedEventArgs args)
         {
-            Team item = (Team)args.SelectedItem;
+            TeamResult item = (TeamResult)args.SelectedItem;
             if (item == null)
             {
                 return;
             }
             App.currTeamNumber = item.TeamNumber;
             App.currTeamName = item.Name;
-            await Navigation.PushAsync(new SelectEventTeamMatchPage(App.currFRCEventKey, item));
+            Team itemTeam = viewModel.DataStoreTeam.GetItemByKeyAsync(item.TeamNumber.ToString()).Result;
+            await Navigation.PushAsync(new SelectEventTeamMatchPage(App.currFRCEventKey, itemTeam));
         }
 
         private void RP_Clicked(object sender, EventArgs e)
@@ -52,6 +55,11 @@ namespace BertScout2019.Views
             AvgScore.BackgroundColor = App.SelectedButtonColor;
             RP.BackgroundColor = App.UnselectedButtonColor;
             viewModel.SortByAverageScore();
+        }
+
+        private void EventTeamsListView_ItemSelected_1(object sender, SelectedItemChangedEventArgs e)
+        {
+
         }
     }
 }
